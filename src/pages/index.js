c5817cd64b745cc3f431a3140232ac595754e92c
Patrick LaRoche <patrick.jr.laroche@gmail.com>
2020-05-14 17:28:05 -0300

    making it my own

--- src/pages/index.js
+++ src/pages/index.js
@@ -282,7 +282,7 @@ const IndexPage = () => (
               <div>
                 Computer Science - Machine Learning and Network Security
               </div>
-              <p>GPA: 3.23</p>
+              <p></p>
             </div>
             <div className="resume-date text-md-right">
               <span className="text-primary"></span>
@@ -296,7 +296,7 @@ const IndexPage = () => (
               <div>
                 Computer Science - Machine Learning and Network Security
               </div>
-              <p>GPA: 3.56</p>
+              <p></p>
             </div>
             <div className="resume-date text-md-right">
               <span className="text-primary">August 2002 - May 2006</span>
@@ -326,65 +326,26 @@ const IndexPage = () => (
         <div className="w-100">
           <h2 className="mb-5">Skills</h2>
 
-          <div className="subheading mb-3">
-            Programming Languages &amp; Tools
-          </div>
-          <ul className="list-inline dev-icons">
-            <li className="list-inline-item">
-              <i className="fab fa-html5"></i>
-            </li>
-            <li className="list-inline-item">
-              <i className="fab fa-css3-alt"></i>
-            </li>
-            <li className="list-inline-item">
-              <i className="fab fa-js-square"></i>
-            </li>
-            <li className="list-inline-item">
-              <i className="fab fa-angular"></i>
-            </li>
-            <li className="list-inline-item">
-              <i className="fab fa-react"></i>
-            </li>
-            <li className="list-inline-item">
-              <i className="fab fa-node-js"></i>
-            </li>
-            <li className="list-inline-item">
-              <i className="fab fa-sass"></i>
-            </li>
-            <li className="list-inline-item">
-              <i className="fab fa-less"></i>
-            </li>
-            <li className="list-inline-item">
-              <i className="fab fa-wordpress"></i>
-            </li>
-            <li className="list-inline-item">
-              <i className="fab fa-gulp"></i>
-            </li>
-            <li className="list-inline-item">
-              <i className="fab fa-grunt"></i>
-            </li>
-            <li className="list-inline-item">
-              <i className="fab fa-npm"></i>
-            </li>
-          </ul>
-
-          <div className="subheading mb-3">Workflow</div>
           <ul className="fa-ul mb-0">
             <li>
               <i className="fa-li fa fa-check"></i>
-              Mobile-First, Responsive Design
+              Expert in Scrum and Kanban, tooling such as Jira, Zenhub, Github,
+              and other code repositories.
             </li>
             <li>
               <i className="fa-li fa fa-check"></i>
-              Cross Browser Testing &amp; Debugging
+              Expert in Terraform, Kubernetes configuration and management, AWS
+              and GKE Cloud, YAML and markdown.
             </li>
             <li>
               <i className="fa-li fa fa-check"></i>
-              Cross Functional Teams
+              Deep knowledge of software pipelines, CI/CD tooling, and
+              implementations including Github Actions, Jenkins, Travis,
+              CirceCi, and similar.
             </li>
             <li>
               <i className="fa-li fa fa-check"></i>
-              Agile Development &amp; Scrum
+              Working knowledge of Go Lang, PHP, and React, C#, Java, Python.
             </li>
           </ul>
         </div>
@@ -398,64 +359,31 @@ const IndexPage = () => (
       >
         <div className="w-100">
           <h2 className="mb-5">Interests</h2>
-          <p>
-            Apart from being a web developer, I enjoy most of my time being
-            outdoors. In the winter, I am an avid skier and novice ice climber.
-            During the warmer months here in Colorado, I enjoy mountain biking,
-            free climbing, and kayaking.
-          </p>
-          <p className="mb-0">
-            When forced indoors, I follow a number of sci-fi and fantasy genre
-            movies and television shows, I am an aspiring chef, and I spend a
-            large amount of my free time exploring the latest technology
-            advancements in the front-end web development world.
-          </p>
+          <ul className="fa-ul mb-0">
+            <li>
+              <i className="fa-li fa fa-check"></i>
+              Outdoor adventure, in particular with my family of four in tow.
+            </li>
+            <li>
+              <i className="fa-li fa fa-check"></i>
+              Fitness and wellbeing, including crossfit and yoga.
+            </li>
+            <li>
+              <i className="fa-li fa fa-check"></i>
+              Travel, exploring our province, and taking in culture abroad.
+            </li>
+            <li>
+              <i className="fa-li fa fa-check"></i>
+              Sailing, an amateur navigator in offshore racing, as well as for
+              leisure.
+            </li>
+          </ul>
         </div>
       </section>
 
       <hr className="m-0" />
 
-      <section
-        className="resume-section p-3 p-lg-5 d-flex align-items-center"
-        id="awards"
-      >
-        <div className="w-100">
-          <h2 className="mb-5">Awards &amp; Certifications</h2>
-          <ul className="fa-ul mb-0">
-            <li>
-              <i className="fa-li fa fa-trophy text-warning"></i>
-              Google Analytics Certified Developer
-            </li>
-            <li>
-              <i className="fa-li fa fa-trophy text-warning"></i>
-              Mobile Web Specialist - Google Certification
-            </li>
-            <li>
-              <i className="fa-li fa fa-trophy text-warning"></i>1<sup>st</sup>
-              Place - University of Colorado Boulder - Emerging Tech Competition
-              2009
-            </li>
-            <li>
-              <i className="fa-li fa fa-trophy text-warning"></i>1<sup>st</sup>
-              Place - University of Colorado Boulder - Adobe Creative Jam 2008
-              (UI Design Category)
-            </li>
-            <li>
-              <i className="fa-li fa fa-trophy text-warning"></i>2<sup>nd</sup>
-              Place - University of Colorado Boulder - Emerging Tech Competition
-              2008
-            </li>
-            <li>
-              <i className="fa-li fa fa-trophy text-warning"></i>1<sup>st</sup>
-              Place - James Buchanan High School - Hackathon 2006
-            </li>
-            <li>
-              <i className="fa-li fa fa-trophy text-warning"></i>3<sup>rd</sup>
-              Place - James Buchanan High School - Hackathon 2005
-            </li>
-          </ul>
-        </div>
-      </section>
+      
     </div>
   </Layout>
 );
